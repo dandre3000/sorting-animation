767c233133c9368e5a2b0fdacc3dbf5313a9cb6f
dandre3000 <deaundre.payne@gmail.com>
2019-10-20 21:15:46 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,7 +11,16 @@ ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canv.width, canv.height);
 
 const dt = 1000 / 60;
+var pause = false;
+
 const data = [];
+const DATA_SIZE = 1000;
+
+const bar = {
+	w: canv.width / DATA_SIZE,
+	maxH: 500
+}
+
 const createData = size => {
 	for (let i = 0; i < size; i++) {
 		const dataPoint = {
@@ -22,7 +31,9 @@ const createData = size => {
 	}
 };
 
-const render = (arr) => {
+const algorithms = new Map();
+
+const render = (arr, idx = null, color = null) => {
 	var set = new Set();
 	if (Array.isArray(arr)) {
 		set = new Set(arr);
@@ -34,7 +45,7 @@ const render = (arr) => {
 		
 		data.forEach((d, idx) => {
 			ctx.fillStyle = 'white';
-			ctx.fillRect(4 * idx, canv.height, canv.width / (data.length * 2), d * -500);
+			ctx.fillRect(bar.w * idx, canv.height, bar.w / 2, d * -500);
 		});
 	}
 	
@@ -46,19 +57,23 @@ const render = (arr) => {
 		ctx.textAlign = 'left';
 		ctx.fillText(timer.getTime(), 16, 16);
 	}
+	
+	if (set.has('bar')) {
+		if (idx !== null && data[idx]) {
+			ctx.fillStyle = 'black';
+			ctx.fillRect(bar.w * idx - 1, canv.height, bar.w / 2 + 2, -501);
+			
+			ctx.fillStyle = color || 'red';
+			ctx.fillRect(bar.w * idx, canv.height, bar.w / 2, data[idx] * -500);
+		}
+	}
 };
 
-/* var elapsedTime = 0;
-var lastTime = 0;
-var loop = null;
-const update = (time = elapsedTime) => {
-	elapsedTime += time - lastTime;
-	render(['timer']);
-	
-	lastTime = time;
-	loop = requestAnimationFrame(update);
-}; */
-const timer = new Timer(0);
+const sleep = ms => {
+	return new Promise(res => setTimeout(res, ms));
+};
+
+const timer = new Timer();
 
 const start = () => {
 	loop = requestAnimationFrame(update);
@@ -88,12 +103,16 @@ Array.prototype.swap = async function(a, b) {
 	}
 };
 
-Array.prototype.isSorted = function(asc = true) {
+Array.prototype.isSorted = async function(asc = true) {
 	for (let i = 0; i < this.length; i++) {
 		if (typeof(this[i]) != 'number') {
 			console.error(`Value of index ${i} is not a number.`);
 			return false;
 		}
+		
+		await sleep(dt);
+		
+		render(['bar'], i, 'green');
 		
 		if (i == this.length - 1) {
 			return true;
@@ -111,36 +130,106 @@ Array.prototype.isSorted = function(asc = true) {
 	}
 };
 
-const quicksort = async (arr, lo, hi) => {
-	if (arr.isSorted()) {
+const asyncSort = async (arr, algorithm) => {
+	if (typeof(algorithm == 'function')) {
+		await algorithm(arr);
 		timer.stop();
+		await arr.isSorted();
 	}
-	
+};
+
+algorithms.set('quicksort', async (arr, lo = 0, hi = arr.length - 1) => {
 	const partition = async () => {
 		const pivot = arr[hi];
 		let i = lo;
 		for (let j = lo; j < hi; j++) {
+			if (pause) {
+				break;
+			}
+			
 			if (arr[j] < pivot) {
 				await arr.swap(i, j);
-				render(['data']);
+				render(['bar'], i);
+				render(['bar'], j);
 				i++;
 			}
 		}
-		await arr.swap(i, hi);
-		render(['data']);
-		return i;
+		
+		if (!pause) {
+			await arr.swap(i, hi);
+			render(['bar'], i);
+			render(['bar'], hi);
+			return i;
+		}
 	};
 	
 	if (lo < hi) {
 		let p = await partition(arr, lo, hi);
-		await quicksort(arr, lo, p - 1);
-		await quicksort(arr, p + 1, hi);
+		await algorithms.get('quicksort')(arr, lo, p - 1);
+		await algorithms.get('quicksort')(arr, p + 1, hi);
 	}
-};
+});
 
-const sleep = ms => {
-	return new Promise(res => setTimeout(res, ms));
-};
+
+algorithms.set('bubblesort', async arr => {
+	var swapped = false;
+	do {
+		if (pause) {
+			break;
+		}
+			
+		swapped = false;
+		for (let i = 1; i < arr.length; i++) {
+			if (arr[i - 1] > arr[i]) {
+				await arr.swap(i - 1, i);
+				render(['bar'], i - 1);
+				render(['bar'], i);
+				swapped = true;
+			}
+		}
+	} while (swapped != false);
+});
+
+algorithms.set('selection sort', async arr => {
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (pause) {
+			break;
+		}
+		
+		let min = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		
+		if (min != i) {
+			await arr.swap(i, min);
+			render(['bar'], i);
+			render(['bar'], min);
+		}
+	}
+});
+
+algorithms.set('insertion sort', async arr => {
+	let i = 1;
+	while (i < arr.length) {
+		if (pause) {
+			break;
+		}
+		
+		let j = i;
+		while (j > 0 && arr[j - 1] > arr[j]) {
+			await arr.swap(j, j - 1);
+			render(['bar'], j);
+			render(['bar'], j - 1);
+			
+			j--;
+		}
+		
+		i++;
+	}
+});
 
 window.onload = () => {
 	$('main').prepend(canv);
@@ -148,18 +237,24 @@ window.onload = () => {
 	
 	const $reset = $('#reset');
 	$reset.click(() => {
-		createData(320);
+		pause = true;
+		timer.reset();
+		createData(DATA_SIZE);
 		render('all');
 	});
 	
 	const $start = $('#start');
 	$start.click(() => {
+		pause = false;
+		
 		timer.start(1000, () => {
 			render(['timer']);
 		});
-		quicksort(data, 0, data.length - 1);
+		
+		let alg = $('#algorithm')[0][$('#algorithm')[0].selectedIndex].value;
+		asyncSort(data, algorithms.get(alg));
 	});
 	
-	createData(320);
+	createData(DATA_SIZE);
 	render('all');
 };
