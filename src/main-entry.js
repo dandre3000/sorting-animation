5a0ba4d955d4c524525734842570063a6a7624ec
dandre3000 <deaundre.payne@gmail.com>
2020-11-05 21:06:02 -0500

    Update main-entry.js

--- src/main-entry.js
+++ src/main-entry.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue';
+import App from './App.vue'
 import store from './store'
-// import './style.css' // extract css
-
-import './assets/play.png'
-import './assets/pause.png'
-import './assets/restart.png'
-import './assets/next.png'
-import './assets/previous.png'
-import './assets/first.png'
-import './assets/last.png'
 
 window.addEventListener('DOMContentLoaded', () => {
 	const main = new Vue({
@@ -22,6 +13,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		e.remove()
 	})
 	
-	window.App = App
 	window.main = main
 })
+
+window.onload = () => {
+	
+}
