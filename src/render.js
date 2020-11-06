33e81fd252d774acf25b5fccc40b2e06e8809487
dandre3000 <deaundre.payne@gmail.com>
2020-11-05 21:06:17 -0500

    Update render.js

--- src/render.js
+++ src/render.js
@@ -1,19 +1,12 @@
-import store from './store'
-
-export const render = () => {
-	const array = store.state.array
-	const sequence = store.state.sequence
-	const canv = store.state.canvas
-	const ctx = canv.getContext('2d')
+export const render = ({ array, canvas, animation }) => {
+	if (!canvas) return
+	
+	const ctx = canvas.getContext('2d')
 	
 	ctx.fillStyle = 'black'
-	ctx.fillRect(0, 0, canv.width, canv.height)
+	ctx.fillRect(0, 0, canvas.width, canvas.height)
 	
 	array.forEach((bar, i) => {
-		bar.render(canv, ctx, sequence[sequence.index], array, i)
+		bar.render(array.length, i, canvas, animation)
 	})
-	
-	ctx.fillStyle = 'white'
-	ctx.fillText(`Comparisons: ${sequence? sequence.comparisons : 0}`, 16, 16)
-	ctx.fillText(`Swaps: ${sequence? sequence.swaps : 0}`, 16, 32)
 }
