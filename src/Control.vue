f2308895307b0ef99a3c05839e8a711d609feb7a
dandre3000 <deaundre.payne@gmail.com>
2020-11-05 21:05:48 -0500

    Separate animation controls from the display

---
+++ src/Control.vue
@@ -0,0 +1,71 @@
+<template>
+	<div>
+		<button id='first' @click='first'><img src='assets/images/first.png'></button>
+		<button id='previous' @click='previous'><img src='assets/images/previous.png'></button>
+		<button id='play' @click='play' v-if='this.$store.state.control == 0'><img src='assets/images/play.png'></button>
+		<button id='pause' @click='pause' v-else-if='this.$store.state.control == 1'><img src='assets/images/pause.png'></button>
+		<button id='restart' @click='restart' v-else><img src='assets/images/restart.png'></button>
+		<button id='next' @click='next'><img src='assets/images/next.png'></button>
+		<button id='last' @click='last'><img src='assets/images/last.png'></button>
+		<input id='descending' @input="toggleDescending" type="checkbox"><label for="descending">Descending</label>
+		<input id='reverse' @input="toggleReverse" type="checkbox"><label for="reverse">Reverse</label>
+		<input id='fps' @input="setFps" type="range" min="1" max="60" value="60" class="slider"><p>Fps: {{ this.$store.state.fps }}</p>
+	</div>
+</template>
+
+<script>
+	import './assets/pause.png'
+	import './assets/play.png'
+	import './assets/restart.png'
+	import './assets/previous.png'
+	import './assets/next.png'
+	import './assets/first.png'
+	import './assets/last.png'
+	
+	export default {
+		methods: {
+			play() {
+				this.$store.dispatch('play')
+			},
+			pause() {
+				this.$store.dispatch('pause')
+			},
+			restart() {
+				this.$store.dispatch('restart')
+			},
+			previous() {
+				this.$store.dispatch('previous')
+			},
+			next() {
+				this.$store.dispatch('next')
+			},
+			first() {
+				this.$store.dispatch('first')
+			},
+			last() {
+				this.$store.dispatch('last')
+			},
+			toggleDescending() {
+				this.$store.dispatch('toggleDescending')
+			},
+			toggleReverse() {
+				this.$store.commit('reverse')
+			},
+			setFps() {
+				this.$store.commit('fps', document.querySelector('#fps').value)
+			}
+		},
+		mounted: function () {
+			const $s = this.$store
+			
+			this.$nextTick(() => {
+			})
+		}
+	}
+</script>
+
+<style>
+	#array-canvas {
+		width: 100%;
+	}
+</style>
--- src/Display.vue
+++ src/Display.vue
@@ -1,77 +1,23 @@
 <template>
 	<div>
-		<canvas id='array-display'></canvas>
-		<div>
-			<p>{{ $store.state.sort.name }}</p>
-			<button @click='first'><img src='assets/images/first.png'></button>
-			<button @click='previous'><img src='assets/images/previous.png'></button>
-			<button id='play' @click='play'>
-				<img src='assets/images/play.png' v-if="this.$store.state.mainBtn == 0">
-				<img src='assets/images/pause.png' v-else-if="this.$store.state.mainBtn == 1">
-				<img src='assets/images/restart.png' v-else>
-			</button>
-			<button @click='next'><img src='assets/images/next.png'></button>
-			<button @click='last'><img src='assets/images/last.png'></button>
-			<input id='descending' @input="toggleDescending" type="checkbox"><label for="descending">Descending</label>
-		</div>
-		<div class='row'>
-			<input id='fps' @input="setFps" type="range" min="1" max="60" value="60" class="slider"><p>Fps: {{ $store.state.fps }}</p>
-		</div>
+		<canvas id='array-canvas'></canvas>
 	</div>
 </template>
 
 <script>
-	import { mapActions } from 'vuex'
-	import { bubbleSort } from './algorithms.js'
-	import { start, stop, step } from './timeStep.js'
-	
 	export default {
-		methods: {
-			first() {
-				this.$store.dispatch('first')
-			},
-			previous() {
-				this.$store.dispatch('previous')
-			},
-			play() {
-				if (this.$store.state.mainBtn == 1) {
-					this.$store.dispatch('pause')
-				} else if (this.$store.state.mainBtn == 0) {
-					this.$store.dispatch('play')
-				} else {
-					this.$store.dispatch('restart')
-				}
-			},
-			next() {
-				this.$store.dispatch('next')
-			},
-			last() {
-				this.$store.dispatch('last')
-			},
-			setFps() {
-				this.$store.dispatch('setFps', document.querySelector('#fps').value)
-			},
-			toggleDescending() {
-				this.$store.dispatch('toggleDescending')
-			}
-		},
 		mounted: function () {
 			const $s = this.$store
 			
-			this.$nextTick(function () {
-				$s.commit('setCanvas', document.querySelector('#array-display'))
+			this.$nextTick(() => {
+				$s.commit('canvas', document.querySelector('#array-canvas'))
 			})
-			
-			window.display = this
 		}
 	}
 </script>
 
 <style>
-	#array-display {
+	#array-canvas {
 		width: 100%;
-	}
-	button:disabled {
-		background-color: black;
 	}
 </style>
